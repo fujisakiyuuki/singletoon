34c878c5b48ec532ff755465a7f966145c67e2d7
LC1D_20_fujisakiyuki <k021g1133@g.neec.ac.jp>
2023-01-25 22:50:18 +0900

    プロジェクト ファイルを追加します。

---
+++ TestSingleton.h
@@ -0,0 +1,13 @@
+#pragma once
+class TestSingleton
+{
+private:
+	TestSingleton() {};
+	~TestSingleton() {};
+	int sceneNo = 1;
+public:
+	TestSingleton(const TestSingleton& obj) = delete;
+	TestSingleton& operator=(const TestSingleton& obj) = delete;
+	static TestSingleton* GetInstance();
+	void ChangeScene(char* Key, char* oldKey);
+};
